ac5a24896f58fffc15581c9ace7b6895898c6676
cathhill <86472730+cathhill@users.noreply.github.com>
2021-09-30 11:01:24 +0100

    Add search form and browser messages

--- public/js/app.js
+++ public/js/app.js
@@ -1,22 +1,29 @@
 console.log("Client side javascript file loaded");
 
-/* fetch(
-  // fetch data from this url and then run this function.
-  "http://puzzle.mead.io/puzzle"
-).then((response) => {
-  //this function will run when the json data has arrived.
-  response.json().then((data) => {
-    console.log(data);
-  });
-}); */
+const weatherForm = document.querySelector("form");
+const search = document.querySelector("input");
+const messageOne = document.querySelector("#message-1");
+const messageTwo = document.querySelector("#message-2");
 
-fetch("http://localhost:3000/weather?address=boston").then((response) => {
-  response.json().then((data) => {
-    if (data.error) {
-      console.log(data.error);
-    } else {
-      console.log(data.location);
-      console.log(data.forecast);
+// "e" short for event.
+weatherForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const location = search.value;
+
+  messageOne.textContent = "Loading...";
+  messageTwo.textContent = "";
+
+  fetch(`http://localhost:3000/weather?address=${location}`).then(
+    (response) => {
+      response.json().then((data) => {
+        if (data.error) {
+          messageOne.textContent = data.error;
+        } else {
+          messageOne.textContent = data.location;
+          messageTwo.textContent = data.forecast;
+        }
+      });
     }
-  });
+  );
 });
